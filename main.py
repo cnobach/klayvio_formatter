3315edd3ae6123bd96dde747e6564fcb52df268b
cnobach <clnobach@gmail.com>
2024-05-30 21:14:20 -0700

    Adding user friendly naming & inputting options

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import shutil
 import os
 import re
 
@@ -17,14 +18,20 @@ def process_csv():
     # List of Files
     dataframes = []
     
+    # List of File Names
+    file_names = []
+    
     for filename in os.listdir(INPUT_DIRECTORY):
         if filename.endswith('.csv'): # checks for existing csv file
             file_path = os.path.join(INPUT_DIRECTORY, filename)
             df = pd.read_csv(file_path)
+            file_names.append(filename.split('.')[0])
             dataframes.append(df)
-            
+    
+    shutil.rmtree(OUTPUT_DIRECTORY);
+    os.mkdir(OUTPUT_DIRECTORY);
+    
     for i, df in enumerate(dataframes):
-        print(f"Shape of dataframe {i}: {df.shape}")
         
         # Check if 'Phone' column exists
         if 'Phone' not in df.columns:
@@ -34,9 +41,12 @@ def process_csv():
         df['Phone'] = df['Phone'].apply(clean_phone_number)
         
         # Save the cleaned data to a new CSV file
-        output_file = os.path.join(OUTPUT_DIRECTORY, f"processed_{i}.csv")
+        output_file = os.path.join(OUTPUT_DIRECTORY, f"{file_names[i]}_processed.csv")
         df.to_csv(output_file, index=False)
         print(f"Processed file saved as {output_file}")
+
+    shutil.rmtree(INPUT_DIRECTORY);
+    os.mkdir(INPUT_DIRECTORY);
 
 if __name__ == "__main__":
     process_csv()
