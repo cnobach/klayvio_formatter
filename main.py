1550a834bab2ca662cb39b9a61706d3c7c83d1d8
cnobach <clnobach@gmail.com>
2024-05-30 22:17:41 -0700

    Adding (very basic) UI to aid with user friendliness

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 import shutil
 import os
 import re
+import ui # Importing the UI file
 
-INPUT_DIRECTORY = './input_files/'
 OUTPUT_DIRECTORY = './output_files/'
 
 def clean_phone_number(phone):
@@ -13,40 +13,44 @@ def clean_phone_number(phone):
     cleaned_phone = '+1' + cleaned_phone
     return cleaned_phone
 
-def process_csv():
+def process_csv(files):
     
     # List of Files
     dataframes = []
     
     # List of File Names
     file_names = []
-    
-    for filename in os.listdir(INPUT_DIRECTORY):
-        if filename.endswith('.csv'): # checks for existing csv file
-            file_path = os.path.join(INPUT_DIRECTORY, filename)
-            df = pd.read_csv(file_path)
-            file_names.append(filename.split('.')[0])
-            dataframes.append(df)
-    
-    shutil.rmtree(OUTPUT_DIRECTORY);
-    os.mkdir(OUTPUT_DIRECTORY);
-    
-    for i, df in enumerate(dataframes):
+    try: 
+        for filename in files:
+            if filename.endswith('.csv'):
+                df = pd.read_csv(filename)
+                names = filename.split('/')
+                file_names.append(names[len(names)-1].split('.')[0])
+                dataframes.append(df)
         
-        # Check if 'Phone' column exists
-        if 'Phone' not in df.columns:
-            raise ValueError("The input CSV does not have a 'Phone' column.")
+        shutil.rmtree(OUTPUT_DIRECTORY);
+        os.mkdir(OUTPUT_DIRECTORY);
         
-        # Clean the phone numbers
-        df['Phone'] = df['Phone'].apply(clean_phone_number)
+        for i, df in enumerate(dataframes):
+            
+            # Check if 'Phone' column exists
+            if 'Phone' not in df.columns:
+                raise ValueError("The input CSV does not have a 'Phone' column.")
+            
+            # Clean the phone numbers
+            df['Phone'] = df['Phone'].apply(clean_phone_number)
+            
+            # Save the cleaned data to a new CSV file
+            output_file = os.path.join(OUTPUT_DIRECTORY, f"{file_names[i]}_processed.csv")
+            df.to_csv(output_file, index=False)
+            print(f"Processed file saved as {output_file}")
+    except: 
+        error = "An error occurred - contact CoCo for support";
+        print(error)
+        return False
+    else:
+        return True
         
-        # Save the cleaned data to a new CSV file
-        output_file = os.path.join(OUTPUT_DIRECTORY, f"{file_names[i]}_processed.csv")
-        df.to_csv(output_file, index=False)
-        print(f"Processed file saved as {output_file}")
-
-    shutil.rmtree(INPUT_DIRECTORY);
-    os.mkdir(INPUT_DIRECTORY);
 
 if __name__ == "__main__":
-    process_csv()
+    ui.createUI() # Call the createUI function from the ui.py file
