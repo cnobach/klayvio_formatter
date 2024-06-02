938d0663c3a8918b34335e62d1b9cf768d899814
cnobach <clnobach@gmail.com>
2024-06-02 08:05:09 -0700

    Removing fluff comments

--- main.py
+++ main.py
@@ -2,24 +2,20 @@ import pandas as pd
 import shutil
 import os
 import re
-import ui # Importing the UI file
+import ui
 
 OUTPUT_DIRECTORY = './output_files/'
 
 def clean_phone_number(phone):
-    # Remove any non-digit characters
     cleaned_phone = re.sub(r'\D', '', phone)
-    # Add +1 at the beginning
     cleaned_phone = '+1' + cleaned_phone
+    
     return cleaned_phone
 
 def process_csv(files):
-    
-    # List of Files
     dataframes = []
-    
-    # List of File Names
     file_names = []
+    
     try: 
         for filename in files:
             if filename.endswith('.csv'):
@@ -32,15 +28,9 @@ def process_csv(files):
         os.mkdir(OUTPUT_DIRECTORY);
         
         for i, df in enumerate(dataframes):
-            
-            # Check if 'Phone' column exists
             if 'Phone' not in df.columns:
                 raise ValueError("The input CSV does not have a 'Phone' column.")
-            
-            # Clean the phone numbers
             df['Phone'] = df['Phone'].apply(clean_phone_number)
-            
-            # Save the cleaned data to a new CSV file
             output_file = os.path.join(OUTPUT_DIRECTORY, f"{file_names[i]}_processed.csv")
             df.to_csv(output_file, index=False)
             print(f"Processed file saved as {output_file}")
@@ -53,4 +43,4 @@ def process_csv(files):
         
 
 if __name__ == "__main__":
-    ui.createUI() # Call the createUI function from the ui.py file
+    ui.createUI()
--- ui.py
+++ ui.py
@@ -3,24 +3,14 @@ import tkinter as tk
 from tkinter import filedialog
 
 def createUI(): 
-    # Create the main window
     window = tk.Tk()
-
-    # Set the window title
     window.title("Klayvio Formatter")
-
-    # Set the window size
     window.geometry("600x400")
 
-    # Start the main event loop
     def select_files():
-        # Open a file dialog to let the user select files
         files = tk.filedialog.askopenfilenames()
-
-        # Pass the selected files to the main.py file
-        success = main.process_csv(files)
+        success = main.process_csv(files)   
         
-        # Display a status message
         if success:
             success_message = tk.Label(window, text="Files processed successfully!", foreground="green")
             success_message.pack(pady=10)
@@ -28,15 +18,12 @@ def createUI():
             error_message = tk.Label(window, text="An error occurred - contact CoCo for support", foreground="red")
             error_message.pack(pady=10)
 
-    # Create a button to trigger file selection
     select_button = tk.Button(window, text="Select Files", command=select_files)
     select_button.pack(pady = 100)
     
-    # Function to close the window
     def close_window():
         window.destroy()
 
-    # Create a button to close the window
     close_button = tk.Button(window, text="Close", command=close_window)
     close_button.pack(pady = 25)
     
