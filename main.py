31739097b35226f99f738b7e16e4992460b39332
cnobach <clnobach@gmail.com>
2024-06-02 08:22:47 -0700

    Changing label & adding warning message

--- main.py
+++ main.py
@@ -15,32 +15,34 @@ def clean_phone_number(phone):
 def process_csv(files):
     dataframes = []
     file_names = []
-    
-    try: 
-        for filename in files:
-            if filename.endswith('.csv'):
-                df = pd.read_csv(filename)
-                names = filename.split('/')
-                file_names.append(names[len(names)-1].split('.')[0])
-                dataframes.append(df)
-        
-        shutil.rmtree(OUTPUT_DIRECTORY);
-        os.mkdir(OUTPUT_DIRECTORY);
-        
-        for i, df in enumerate(dataframes):
-            if 'Phone' not in df.columns:
-                raise ValueError("The input CSV does not have a 'Phone' column.")
-            df['Phone'] = df['Phone'].apply(clean_phone_number)
-            output_file = os.path.join(OUTPUT_DIRECTORY, f"{file_names[i]}_processed.csv")
-            df.to_csv(output_file, index=False)
-            print(f"Processed file saved as {output_file}")
-    except: 
-        error = "An error occurred - contact CoCo for support";
-        print(error)
-        return False
+
+    if(len(files) > 0):
+        try: 
+            for filename in files:
+                if filename.endswith('.csv'):
+                    df = pd.read_csv(filename)
+                    names = filename.split('/')
+                    file_names.append(names[len(names)-1].split('.')[0])
+                    dataframes.append(df)
+            
+            shutil.rmtree(OUTPUT_DIRECTORY);
+            os.mkdir(OUTPUT_DIRECTORY);
+            
+            for i, df in enumerate(dataframes):
+                if 'Phone' not in df.columns:
+                    raise ValueError("The input CSV does not have a 'Phone' column.")
+                df['Phone'] = df['Phone'].apply(clean_phone_number)
+                output_file = os.path.join(OUTPUT_DIRECTORY, f"{file_names[i]}_processed.csv")
+                df.to_csv(output_file, index=False)
+                print(f"Processed file saved as {output_file}")
+        except: 
+            error = "An error occurred - contact CoCo for support";
+            print(error)
+            return 2 # If an error occurred
+        else:
+            return 1 # If files were processed successfully
     else:
-        return True
-        
+        return 0 # If no files were selected
 
 if __name__ == "__main__":
     ui.createUI()
--- ui.py
+++ ui.py
@@ -6,17 +6,20 @@ def createUI():
     window = tk.Tk()
     window.title("Klayvio Formatter")
     window.geometry("600x400")
+    
+    message_label = tk.Label(window, text="Select the CSV files you would like to process", font=("Arial", 16))
+    message_label.pack(pady=10)
 
     def select_files():
         files = tk.filedialog.askopenfilenames()
         success = main.process_csv(files)   
-        
-        if success:
-            success_message = tk.Label(window, text="Files processed successfully!", foreground="green")
-            success_message.pack(pady=10)
-        else:
-            error_message = tk.Label(window, text="An error occurred - contact CoCo for support", foreground="red")
-            error_message.pack(pady=10)
+
+        if success == 2:
+            message_label.config(text="An error occurred - contact CoCo for support", foreground="red")
+        elif success == 1:
+            message_label.config(text="Files processed successfully!", foreground="green")
+        elif success == 0:
+            message_label.config(text="Please select files for processing", foreground="orange")
 
     select_button = tk.Button(window, text="Select Files", command=select_files)
     select_button.pack(pady = 100)
